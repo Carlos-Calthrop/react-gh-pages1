5d99d9e11510824f8f4e4d41113201064fd1163f
carlos <Carlos.Calthrop@odysseuslabs.io>
2020-08-03 07:55:47 -0400

    Updates

---
+++ precache-manifest.8bc49dabc0467cf6fd711060f72552f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7248f3cd59cf0ff0eee9ca38b9bab9",
+    "url": "/react-gh-pages1/index.html"
+  },
+  {
+    "revision": "b466af89ee8eae796ba2",
+    "url": "/react-gh-pages1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/react-gh-pages1/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages1/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b466af89ee8eae796ba2",
+    "url": "/react-gh-pages1/static/js/main.6530229d.chunk.js"
+  },
+  {
+    "revision": "b1b4ab36b2e0f75f7ecf",
+    "url": "/react-gh-pages1/static/js/runtime-main.7fb41f27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages1/static/media/logo.5d5d9eef.svg"
+  }
+]);
